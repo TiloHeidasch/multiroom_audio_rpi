1f9786b1c38cbc4f5a0aa1121d53fd93d203c281
Tilo Heidasch <tilo.heidasch@capgemini.com>
2020-03-12 23:35:27 +0100

    Added necessary Bluetooth isntructions

--- installServer.sh
+++ installServer.sh
@@ -8,13 +8,78 @@ wget https://github.com/badaix/snapcast/releases/download/v0.17.1/snapserver_0.1
 sudo dpkg -i --force-all snapserver_0.17.1-1_armhf.deb &&
 sudo apt-get -f install -y &&
 
+# Bluetooth Install
+wget -q https://github.com/nicokaiser/rpi-audio-receiver/archive/master.zip
+unzip master.zip
+rm master.zip
+
+cd rpi-audio-receiver-master
+./install.sh
+cd ..
+
+# Configure Bluetooth
+## Add Bluetooth User
+sudo adduser blueaudio
+## Add to all groups
+sudo usermod -a -G adm
+sudo usermod -a -G dialout
+sudo usermod -a -G cdrom
+sudo usermod -a -G sudo
+sudo usermod -a -G audio
+sudo usermod -a -G video
+sudo usermod -a -G plugdev
+sudo usermod -a -G games
+sudo usermod -a -G users
+sudo usermod -a -G input
+sudo usermod -a -G netdev
+sudo usermod -a -G spi
+sudo usermod -a -G gpio
+
+## Configure a2dp agent to use blueaudio
+echo "[Unit]
+Description=Bluetooth A2DP Agent
+Requires=bluetooth.service
+After=bluetooth.service
+
+[Service]
+ExecStart=/usr/local/bin/a2dp-agent.py
+RestartSec=5
+Restart=always
+User=blueaudio
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /etc/systemd/system/a2dp-agent.service
+
+## Configure Aplay Service to use blueaudio
+echo "[Unit]
+Description=BlueALSA aplay
+Requires=bluealsa.service
+After=bluealsa.service sound.target
+
+[Service]
+Type=simple
+User=root
+ExecStartPre=/bin/sleep 2
+ExecStart=/usr/bin/bluealsa-aplay --pcm-buffer-time=250000 00:00:00:00:00:00
+RestartSec=5
+Restart=always
+User=blueaudio
+
+[Install]
+WantedBy=multi-user.target
+" | sudo tee /etc/systemd/system/bluealsa-aplay.service
+
+## Disable Onboard Bluetooth
+echo "dtoverlay=pi3-disable-bt" | sudo tee -a /boot/config.txt
+
+
 # Configure Raspotify
 echo 'DEVICE_NAME="HOME" 
 BITRATE="320" 
 BACKEND_ARGS="--backend pipe --device /tmp/snapfifo"' | sudo tee /etc/default/raspotify
 
 # Configure Snapserver
-sudo cp config/snapserver.conf /etc/snapserver.conf
 echo 'SNAPSERVER_OPTS="-d"' | sudo tee /etc/default/snapserver
 
 # Create Service routine
@@ -32,8 +97,8 @@ WantedBy=multi-user.target
 # Start new service routine
 sudo systemctl enable multiroom_audio_rpi_server.service
 
-# Deactivate Raspotify
-sudo systemctl disable raspotify.service
+# Reboot Raspotify
+sudo systemctl restart raspotify.service
 
 # Start new service routine
 sudo systemctl start multiroom_audio_rpi_server.service
